7a778102e86ce79a602552f56bb4440ea225294b
ZanderKirilov <biross11@gmail.com>
2017-09-22 23:15:49 +0300

    Commit added functionality to HomeScreen, fixed layout

---
+++ JavaForEveryone3/app/src/main/java/com/example/sethcohen/javaforeveryone3/ConditionsAndLoops.java
@@ -0,0 +1,13 @@
+package com.example.sethcohen.javaforeveryone3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ConditionsAndLoops extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_conditions_and_loops);
+    }
+}
--- JavaForEveryone3/app/src/main/java/com/example/sethcohen/javaforeveryone3/HomeScreen.java
+++ JavaForEveryone3/app/src/main/java/com/example/sethcohen/javaforeveryone3/HomeScreen.java
@@ -3,8 +3,14 @@ package com.example.sethcohen.javaforeveryone3;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class HomeScreen extends AppCompatActivity {
 
@@ -13,6 +19,9 @@ public class HomeScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
         gotoIntroduction();
+        gotoConditionsAndLoops();
+        gotoArrays();
+        //showIntro();
     }
 
     public void gotoIntroduction(){
@@ -20,8 +29,54 @@ public class HomeScreen extends AppCompatActivity {
         registerPage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                                            //Ако тук смениш Introduction.class със Register.class работи!
+
                 Intent register = new Intent(HomeScreen.this, Introduction.class);
+                startActivity(register);
+            }
+        });
+    }
+
+    TextView textView;
+
+//    public void showIntro(){
+//        textView = (TextView) findViewById(R.id.text_Intro);
+//        textView.setMovementMethod(new ScrollingMovementMethod());
+//        String data = "";
+//        StringBuffer sBuffer = new StringBuffer();
+//        InputStream is = this.getResources().openRawResource(R.raw.intro);
+//        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+//        if(is != null){
+//            try{
+//                while ((data = reader.readLine()) != null){
+//                    sBuffer.append(data + "n");
+//                }
+//                textView.setText(sBuffer);
+//                is.close();
+//            } catch (Exception ex) {
+//                ex.printStackTrace();
+//            }
+//        }
+//    }
+
+    public void gotoConditionsAndLoops(){
+        Button registerPage = (Button) findViewById(R.id.button2_Loops);
+        registerPage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent register = new Intent(HomeScreen.this, ConditionsAndLoops.class);
+                startActivity(register);
+            }
+        });
+    }
+
+    public void gotoArrays(){
+        Button registerPage = (Button) findViewById(R.id.button3_Arrays);
+        registerPage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent register = new Intent(HomeScreen.this, Arrays.class);
                 startActivity(register);
             }
         });
