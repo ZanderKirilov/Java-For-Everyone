a6b4434601a41a12b1701d7f3626b505b530499e
ValeryJordanov <v.iordanov@paip3.com>
2017-09-22 19:45:58 +0300

    Testing button on HomeScreen

--- JavaForEveryone3/app/src/main/java/com/example/sethcohen/javaforeveryone3/HomeScreen.java
+++ JavaForEveryone3/app/src/main/java/com/example/sethcohen/javaforeveryone3/HomeScreen.java
@@ -1,7 +1,10 @@
 package com.example.sethcohen.javaforeveryone3;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomeScreen extends AppCompatActivity {
 
@@ -9,5 +12,20 @@ public class HomeScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+        gotoIntroduction();
     }
+
+    public void gotoIntroduction(){
+        Button registerPage = (Button) findViewById(R.id.button1_Intro);
+        registerPage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                                            //Ако тук смениш Introduction.class със Register.class работи!
+                Intent register = new Intent(HomeScreen.this, Introduction.class);
+                startActivity(register);
+            }
+        });
+    }
+
+
 }
