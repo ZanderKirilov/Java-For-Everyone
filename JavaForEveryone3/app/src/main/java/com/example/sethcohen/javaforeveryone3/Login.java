2213bb23cc189625e36efcffdbbfcbc518b6351e
ValeryJordanov <v.iordanov@paip3.com>
2017-09-22 15:20:09 +0300

    LoginForm fixed and allows you to enter HomeScreen with user:admin and pass:admin

--- JavaForEveryone3/app/src/main/java/com/example/sethcohen/javaforeveryone3/Login.java
+++ JavaForEveryone3/app/src/main/java/com/example/sethcohen/javaforeveryone3/Login.java
@@ -5,22 +5,44 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
+    Button login;
+    EditText username;
+    EditText password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         registerButton();
+        login = (Button)findViewById(R.id.btn_Login_Login);
+        username = (EditText)findViewById(R.id.editTextEmail);
+        password = (EditText)findViewById(R.id.editTextPassword);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                login();
+            }
+        });
 
     }
 
-    public void loginButton(View view){
-        Intent testing = new Intent(Login.this, HomeScreen.class);
-        startActivity(testing);
-    }
+    public void login(){
+        String user = username.getText().toString().trim();
+        String pass = password.getText().toString().trim();
+        if(user.equals("admin") && pass.equals("admin")){
+            Toast.makeText(this, "Processing", Toast.LENGTH_LONG).show();
 
+            Intent testing = new Intent(Login.this, HomeScreen.class);
+            startActivity(testing);
+        }else{
+            Toast.makeText(this,"Wrong input", Toast.LENGTH_LONG).show();
+        }
+
+    }
 
     public void registerButton(){
         Button registerPage = (Button) findViewById(R.id.btn_RegisterHome_Login);
